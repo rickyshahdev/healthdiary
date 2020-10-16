6a63ed72451b3bf9158840359dfc19e07274b665
John Muhlenberg <jmuhlenberg16@yahoo.com>
2020-10-16 17:37:45 -0400

    corrected new image not rendering correctly

--- app.js
+++ app.js
@@ -1,86 +1,87 @@
-
-
-
 class App extends React.Component {
   state= {
     diary: []
   }
 
   componentDidMount = () => {
-        axios.get('/healthdir').then(
-          (response) => {
-           this.setState ({
-             diary: response.data
-           })
-        })
-      }
+    axios.get('/healthdir').then(
+      (response) => {
+       this.setState ({
+         diary: response.data
+       })
+    })
+  }
+
   changeEventTitle = (event) => {
     this.setState({
         newEventTitle:event.target.value
     })
   }
 
-changeEventDate = (event) => {
+  changeEventDate = (event) => {
     this.setState({
         newEventDate:event.target.value
     })
-}
-changeEventDescription = (event) => {
+  }
+
+  changeEventDescription = (event) => {
     this.setState({
         newEventDescription:event.target.value
     })
-}
-changeEventImage = (event) => {
+  }
+
+  changeEventImage = (event) => {
     this.setState({
         newEventImage:event.target.value
     })
-}
+  }
 
-createEvent = (event) => {
+  createEvent = (event) => {
     event.preventDefault();
     axios.post('/healthdir',
     {
       title: this.state.newEventTitle,
       date: this.state.newEventDate,
       description: this.state.newEventDescription,
-      image: this.state.newEventimage
+      image: this.state.newEventImage
     }).then(
       (response) => {
-          this.setState({
-              diary: response.data
+        this.setState({
+          diary: response.data
         })
       })
-}
+    }
     render = () => {
         return <div>
-            <h2>Add Event</h2>
-            <form onSubmit={this.createEvent}>
-                <input onKeyUp={this.changeEventTitle} type="text" placeholder="Title"/><br/>
-                <input onKeyUp={this.changeEventDate} type="datetime" placeholder="eg: 2020-08-14 06:30:15"/><br/>
-                <input onKeyUp={this.changeEventDescription} type="text" placeholder="Description"/><br/>
-                <input onKeyUp={this.changeEventImage} type="text" placeholder="image link"/><br/>
-                <input type="submit" value="Add Event"/>
-            </form>
-            <h2> List of Events </h2>
-            <ul>
-              {
-                this.state.diary.map(
-                  (event) => {
-                    return <li>
-                    <img className= "img"src={event.image} alt="image"/>
-                    <h5> Title: {event.title}</h5>
-                    <h5> Date : {event.date} </h5>
-                    <h5> Description: {event.description}</h5>
+          <h2>Add Event</h2>
+          <form onSubmit={this.createEvent}>
+            <input onKeyUp={this.changeEventTitle} type="text" placeholder="Title"/><br/>
+            <input onKeyUp={this.changeEventDate} type="date" placeholder="eg: 2020-08-14 06:30:15"/><br/>
+            <input onKeyUp={this.changeEventImage} type="text" placeholder="image link"/><br/>
+            <input onKeyUp={this.changeEventDescription} type="textarea" placeholder="Description"/><br/>
 
-                    </li>
-                  }
-                )
-              }
-            </ul>
+            <input type="submit" value="Add Event"/>
+          </form>
+          <h2> List of Events </h2>
+          <ul>
+            {
+              this.state.diary.map(
+                (event) => {
+                  return <li key={event.id}>
+                  <img className= "img"  src={event.image} alt={event.title}/>
+                  <h5> Title: {event.title}</h5>
+                  <h5> Date : {event.date} </h5>
+                  <h5> Description: {event.description}</h5>
+                  </li>
+                }
+              )
+            }
+          </ul>
         </div>
     }
 
 }
+
 ReactDOM.render(
     <App></App>,
     document.querySelector('main')
