6856976f12c0a170943d09e15bfd7d67eda6650c
Ricky Shah <ricky@Rickys-MacBook-Pro.local>
2020-10-18 13:05:26 -0400

    looks nice

---
+++ login/login.js
@@ -0,0 +1,32 @@
+class Login extends React.Component{
+  state ={
+    people: []
+  }
+  getPeople = () => {
+    console.log(this.state);
+
+    axios.get('/users').then((response) => {
+        this.setState({
+        people: response.data
+      })
+    })
+}
+
+render = () => {
+  return(
+    <div>
+    <form action="/diary/index.html" onSubmit={this.getPeople} method="POST">
+      <input type="text" placeholder="username"/><br/>
+      <input type="text" placeholder="password"/><br/>
+      <input type="submit" value="Sign In"/>
+    </form>
+    </div>
+  )
+}
+
+}
+
+ReactDOM.render(
+  <Login />,
+  document.querySelector('main')
+);
